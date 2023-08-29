4f473240a384db7d57af3b40d3ef1c0eddca2cee
VictorPera <victorhugo.pera@gmail.com>
2023-08-29 16:35:35 -0300

    ecomerce dinamico  criado

---
+++ main.js
@@ -0,0 +1,69 @@
+const catalogo = [
+    {
+        id: 1,
+        nome : 'Box Senhor dos Anéis + O Hobbit',
+        preco : 165,
+        imagem: 'produto-1.jpg',
+    },
+    {
+        id: 2,
+        nome : 'Box Harry Potter completa',
+        preco : 365,
+        imagem: 'produto-2.jpg',
+    },
+    {
+        id: 3,
+        nome: 'Box Jogos Vorazes completa',
+        preco: 99.99,
+        imagem: 'produto-3.jpg',
+    },
+    {
+        id: 4,
+        nome: 'Box Trono de Vidro completa',
+        preco: 199.99,
+        imagem: 'produto-4.jpg',
+    },
+    {
+        id: 5,
+        nome: 'As Crônicas de Gelo e Fogo (Guerra dos Tronos)',
+        preco: 550,
+        imagem: 'produto-5.jpg',
+    },
+    {
+        id: 6,
+        nome: 'Diário De Um Banana - 10 Volumes',
+        preco: 288.50, 
+        imagem: 'produto-6.jpg',
+    },
+    {
+        id: 7,
+        nome: 'Box Arsène Lupin Editora Principis - 7 volumes',
+        preco: 84.99, 
+        imagem: 'produto-7.jpg',
+    },
+    {
+        id: 8,
+        nome: 'Box The Witcher',
+        preco: 350,
+        imagem: 'produto-8.jpg',
+    },
+];
+
+for(const produtoCatalogo of catalogo){
+
+const cartaoProduto = `
+<div id="card-produto">
+    <img 
+    src="./img/${produtoCatalogo.imagem}" 
+    alt="Imagem da Box Senhor Dos Anéis + O Hobbit"
+    style="height: 360px"
+    >
+    <h2>${produtoCatalogo.nome}</h2>
+    <p>R&#x24; ${produtoCatalogo.preco}</p>
+    <button>Adicionar ao carrinho</button>
+</div>
+`;
+
+document.getElementById("container-produto").innerHTML += cartaoProduto;
+};
+
