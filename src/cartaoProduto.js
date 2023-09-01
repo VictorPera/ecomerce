08af5c2ae5d2a469a1e6dd89cace466ea6df442f
VictorPera <victorhugo.pera@gmail.com>
2023-09-01 16:10:59 -0300

    progresso no carrinho css+js

---
+++ src/cartaoProduto.js
@@ -0,0 +1,24 @@
+import { catalogo } from "./utilidades";
+import { adicionarAoCarrinho } from './menuCarrinho';
+
+export function renderizarCatalogo(){
+    for(const produtoCatalogo of catalogo){
+
+        const cartaoProduto = `
+        <div id="card-produto-${produtoCatalogo.id}" class="flex flex-col w-80 m-2 p-2 justify-between shadow-xl shadow-slate-400 rounded-lg group">
+            <img 
+            src="./img/${produtoCatalogo.imagem}"
+            alt="${produtoCatalogo.nome}"
+            class="group-hover:scale-110 duration-300 my-3 w-[290px] self-center " 
+            >
+            <h2 class="text-sm">${produtoCatalogo.nome}</h2>
+            <p class="text-lg">R&#x24; ${produtoCatalogo.preco}</p>
+            <button id="adicionar-${produtoCatalogo.id}" class="bg-slate-950 text-slate-200 hover:bg-slate-700 duration-200 rounded-lg"><i class="fa-solid fa-cart-plus"></i></button>
+        </div>`;
+        document.getElementById("container-produto").innerHTML += cartaoProduto;
+    };
+
+    for(const produtoCatalogo of catalogo){
+        document.getElementById(`adicionar-${produtoCatalogo.id}`).addEventListener('click', adicionarAoCarrinho);
+    }
+}
--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -15,3 +15,17 @@ export function inicializarCarrinho(){
     botaoFecharCarrinho.addEventListener('click', fechaCarrinho);
     botaoAbrirCarrinho.addEventListener('click', abrirCarrinho);
 }
+
+export function adicionarAoCarrinho(){
+    const containerProdutosCarrinho = document.getElementById("produtos-carrinho");
+    const cartaoProdutoCarrinho = `
+    <article class="flex bg-slate-100 rounded-lg relative">
+        <button id="fechar-carrinho"><i class="fa-solid fa-circle-xmark text-slate-500 absolute right-[10px] top-[50px] hover:text-slate-900 duration-300"></i></button>
+        <img src="./img/produto-1.jpg" alt="Carrinho: " class="w-24 rounded-lg p-2">
+        <div>
+            <p class="text-slate-900 text-sm">As Crônicas de Gelo e Fogo (Guerra dos Tronos)</p>
+            <p class="text-green-800 text-lg">Preço</p>
+        </div>
+    </article>`;
+    containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
+}
